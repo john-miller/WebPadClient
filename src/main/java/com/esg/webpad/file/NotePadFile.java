d5ca8614266b6fe866855291a0de8d43a2d407ee
Jonathan Miller <jonathan@winepos.com>
2016-06-15 15:36:51 -0400

    Represents a webpad file

---
+++ src/main/java/com/esg/webpad/file/NotePadFile.java
@@ -0,0 +1,42 @@
+package com.esg.webpad.file;
+
+public class NotePadFile {
+
+	private String name;
+	private String description;
+	private String encryptedData;
+	private transient String content;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getEncryptedData() {
+		return encryptedData;
+	}
+
+	public void setEncryptedData(String data) {
+		this.encryptedData = data;
+	}
+	
+}
