2431882d4be81e2216ab9a53595b27d27e76a350
Jonathan Miller <jonathan@winepos.com>
2016-06-16 17:14:32 -0400

    Added call to set menu items

--- src/main/java/com/esg/webpad/controller/NewFileController.java
+++ src/main/java/com/esg/webpad/controller/NewFileController.java
@@ -2,7 +2,12 @@ package com.esg.webpad.controller;
 
 import org.apache.log4j.Logger;
 
+import com.esg.webpad.menu.FileMenuBarItem;
+import com.esg.webpad.menu.OptionsMenuBarItem;
 import com.esg.webpad.service.UIService;
+import com.esg.webpad.view.ApplicationFrame;
+import com.esg.webpad.view.FXApplicationFrame;
+import com.esg.webpad.view.SwingApplicationFrame;
 import com.esg.webpad.view.newfile.FXNewFileForm;
 import com.esg.webpad.view.newfile.NewFileForm;
 import com.esg.webpad.view.newfile.SwingNewFileForm;
@@ -13,12 +18,16 @@ public enum NewFileController {
 	
 	private Logger logger = Logger.getLogger(NewFileController.class);
 	private NewFileForm fileForm = UIService.INSTANCE.isJavaFX() ? FXNewFileForm.INSTANCE : SwingNewFileForm.INSTANCE;
+	private ApplicationFrame<?> applicationFrame = UIService.INSTANCE.isJavaFX() ? FXApplicationFrame.INSTANCE : SwingApplicationFrame.INSTANCE;
 	
 	private NewFileController() {
 		logger.info("New File Controller Created");
 	}
 	
 	public void newFile() {
+		
+		applicationFrame.setMenuBarItems(FileMenuBarItem.INSTANCE, OptionsMenuBarItem.INSTANCE);
+		
 		fileForm.display();
 	}
 
