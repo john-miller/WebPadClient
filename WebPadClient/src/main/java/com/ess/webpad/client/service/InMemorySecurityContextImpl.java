57ec8cd78a30c867f6fd748c6255f52f3ce4940f
Jonathan Miller <jonmiller44@gmail.com>
2017-01-14 20:38:10 -0500

    Added security context with in memory implementation

---
+++ WebPadClient/src/main/java/com/ess/webpad/client/service/InMemorySecurityContextImpl.java
@@ -0,0 +1,25 @@
+package com.ess.webpad.client.service;
+
+import com.ess.webpad.client.domain.Account;
+
+/**
+ * Holds the currently authenticated user in memory
+ *
+ * Created On Jan 14, 2017
+ * @author Jonathan Miller
+ */
+public enum InMemorySecurityContextImpl implements SecurityContext {
+	
+	INSTANCE;
+	
+	private Account currentAccount;
+
+	public Account getPrincipal() {
+		return currentAccount;
+	}
+
+	public void setPrincipal(Account account) {
+		this.currentAccount = account;
+	}
+
+}
