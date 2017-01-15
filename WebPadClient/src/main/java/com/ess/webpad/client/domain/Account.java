ec7f52ab2a46ddde50480d36a125fbe70667f174
Jonathan Miller <jonmiller44@gmail.com>
2017-01-14 20:37:47 -0500

    Added domain objects

---
+++ WebPadClient/src/main/java/com/ess/webpad/client/domain/Account.java
@@ -0,0 +1,16 @@
+package com.ess.webpad.client.domain;
+
+public class Account {
+	
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+
+}
