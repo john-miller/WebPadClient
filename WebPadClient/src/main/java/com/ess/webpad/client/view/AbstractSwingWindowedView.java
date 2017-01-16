eb6780095c18e7dc0cf136b8ad02500bd25e2fda
Jonathan Miller <jonmiller44@gmail.com>
2017-01-16 07:40:06 -0500

    Added windowed view with listener and implementations

---
+++ WebPadClient/src/main/java/com/ess/webpad/client/view/AbstractSwingWindowedView.java
@@ -0,0 +1,54 @@
+package com.ess.webpad.client.view;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public abstract class AbstractSwingWindowedView implements WindowedView {
+	
+	private JFrame frmMain = new JFrame();
+	private WindowedViewListener windowedViewListener;
+	
+	public void initialize() {
+		frmMain.add(getReferencePanel());
+		frmMain.pack();
+	}
+	
+	@Override
+	public void displayInWindow() {
+		frmMain.setVisible(true);
+	}
+
+	@Override
+	public void closeWindow() {
+		frmMain.dispose();
+	}
+
+	public void setWindowedViewListener(WindowedViewListener windowedViewListener) {
+		this.windowedViewListener = windowedViewListener;
+	}
+
+	public WindowedViewListener getWindowedViewListener() {
+		return windowedViewListener;
+	}
+		
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setTitle(String title) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setDimension(double width, double height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public abstract JPanel getReferencePanel();
+
+}
---
+++ WebPadClient/src/main/java/com/ess/webpad/client/view/WindowedViewListener.java
@@ -0,0 +1,7 @@
+package com.ess.webpad.client.view;
+
+public interface WindowedViewListener {
+	
+	public void onClose();
+	
+}
