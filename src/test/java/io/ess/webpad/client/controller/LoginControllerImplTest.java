fbfc4f6d48f2f1962df19f26cc9cb2bd6a662c15
jmiller <jmiller@localhost>
2017-01-30 22:21:31 -0500

    Added login controller test

---
+++ src/test/java/io/ess/webpad/client/controller/LoginControllerImplTest.java
@@ -0,0 +1,14 @@
+package io.ess.webpad.client.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginControllerImplTest {
+	
+	@Test
+	public void instantiate() {
+		assertNotNull(new LoginControllerImpl());
+	}
+
+}
