57da8dfb64f0ceb78f374dd36bf95a4c8370a384
jmiller <jmiller@localhost>
2017-01-30 22:53:27 -0500

    Polish

--- src/main/java/io/ess/webpad/client/service/JVMService.java
+++ src/main/java/io/ess/webpad/client/service/JVMService.java
@@ -4,9 +4,17 @@ public enum JVMService {
 	
 	INSTANCE;
 	
+	public static final String JAVAFX_INDICATOR_CLASS = "com.sun.javafx.application.PlatformImpl";
+	
+	/**
+	 * Will attempt to get the JAVAFX_INDICATOR_CLASS
+	 * by name.  If it succeeds, it is very likely that the client
+	 * supports JavaFX
+	 * @return
+	 */
 	public boolean supportsJavaFX() {
 		try {
-			Class.forName("com.sun.javafx.application.PlatformImpl");
+			Class.forName(JAVAFX_INDICATOR_CLASS);
 			return true;
 		} catch(ClassNotFoundException e) {
 			return false;
