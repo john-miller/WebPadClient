d137264ced15163a1c2bb6fbcba2f9fe0ca1cfd3
jmiller <jmiller@localhost>
2017-01-31 23:00:16 -0500

    Implemented Editor View

--- src/main/java/io/ess/webpad/client/editor/FXEditorViewImpl.java
+++ src/main/java/io/ess/webpad/client/editor/FXEditorViewImpl.java
@@ -1,15 +1,13 @@
 package io.ess.webpad.client.editor;
 
+import io.ess.webpad.client.domain.Document;
 import io.ess.webpad.client.view.AbstractFXWindowedView;
 
-public class FXEditorViewImpl extends AbstractFXWindowedView {
+public class FXEditorViewImpl extends AbstractFXWindowedView implements EditorView {
 
-	@Override
-	public void setDimension(double width, double height) {
-		// TODO Auto-generated method stub
-		
-	}
-
+	private Document document;
+	private EditorViewListener editorViewListener;
+	
 	@Override
 	public String getFXMLDocument() {
 		return "/fxml/editor-view.fxml";
@@ -18,6 +16,26 @@ public class FXEditorViewImpl extends AbstractFXWindowedView {
 	@Override
 	public String getCSSLocation() {
 		return "/css/editor-view.css";
+	}
+
+	@Override
+	public void setDocument(Document document) {
+		this.document = document;
+	}
+
+	@Override
+	public Document getDocument() {
+		return this.document;
+	}
+
+	@Override
+	public void setEditorViewListener(EditorViewListener editorViewListener) {
+		this.editorViewListener = editorViewListener;
+	}
+
+	@Override
+	public EditorViewListener getEditorViewListener() {
+		return this.editorViewListener;
 	}
 
 }
