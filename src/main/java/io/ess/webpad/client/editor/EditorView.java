b6882b37dc8dc9f29ae5015c918832ede983ff89
jmiller <jmiller@localhost>
2017-01-31 22:40:20 -0500

    Polish

--- src/main/java/io/ess/webpad/client/editor/EditorView.java
+++ src/main/java/io/ess/webpad/client/editor/EditorView.java
@@ -2,6 +2,14 @@ package io.ess.webpad.client.editor;
 
 import io.ess.webpad.client.domain.Document;
 
+/**
+ * The editor view is responsible for providing
+ * a UI to the user, allowing them to perform operations
+ * on the document and other parts of the application
+ * 
+ * @author John Miller
+ *
+ */
 public interface EditorView {
 	
 	public void setDocument(Document document);
