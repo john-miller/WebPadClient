f06abbf122ec1dcddc183a6fd8616b8708417590
Jonathan Miller <jonmiller44@gmail.com>
2017-02-05 10:34:27 -0500

    Added methods

--- src/main/java/io/ess/webpad/client/rest/DocumentRestClient.java
+++ src/main/java/io/ess/webpad/client/rest/DocumentRestClient.java
@@ -1,5 +1,18 @@
 package io.ess.webpad.client.rest;
 
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resource;
+
+import io.ess.webpad.client.domain.Document;
+
 public interface DocumentRestClient {
+	
+	public PagedResources<Resource<Document>> getDocuments(int page, int size);
+	
+	public Resource<Document> getFullDocument(Resource<Document> documentResource);
+	
+	public Resource<Document> getDocumentByName(String name);
+	
+	public Resource<Document> updateDocument(Resource<Document> document);
 
 }
