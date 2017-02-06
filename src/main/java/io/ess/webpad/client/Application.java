8917bbc50a985f0809f6f56e386b3409ad15870f
Jonathan Miller <jonmiller44@gmail.com>
2017-02-05 23:16:15 -0500

    Added test document

--- src/main/java/io/ess/webpad/client/Application.java
+++ src/main/java/io/ess/webpad/client/Application.java
@@ -1,13 +1,17 @@
 package io.ess.webpad.client;
 
-import io.ess.webpad.client.login.LoginController;
-import io.ess.webpad.client.login.LoginControllerImpl;
+import io.ess.webpad.client.domain.Document;
+import io.ess.webpad.client.editor.EditorController;
+import io.ess.webpad.client.editor.EditorControllerImpl;
 
 public class Application {
 
 	public static void main(String[] args) {
-		LoginController loginController = new LoginControllerImpl();
-		loginController.login();
+		Document document = new Document();
+		document.setName("Test Document");
+		document.setContent("<b>sample content</b>");
+		EditorController loginController = EditorControllerImpl.INSTANCE;
+		loginController.openDocument(document);
 	}
 	
 }
